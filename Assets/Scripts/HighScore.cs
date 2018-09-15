ac37864cf710f5ce041663fc9b05dc30ae80d065
Adrian Khashaki <Adrian.Khashaki@fluffyfairygames.com>
2018-09-15 22:57:29 +0200

    Added Highscore functionality

--- Assets/Scripts/HighScore.cs
+++ Assets/Scripts/HighScore.cs
@@ -1,16 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HighScore : MonoBehaviour {
+public class HighScore : MonoBehaviour
+{
+    [SerializeField] private Text _namesText;
+    [SerializeField] private Text _scoresText;
+    [SerializeField] private Menu _mainMenuPrefab;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Start()
+    {
+        StateController.AddHighScore(Random.Range(0, 9999999).ToString(), Random.Range(0, 9999999)); //Delete This
+        var highScore = StateController.HighScore;
+        var nameStringBuilder = new StringBuilder();
+        var scoreStringBuilder = new StringBuilder();
+        for (var i = 0; i < highScore.Length; i++)
+        {
+            var scoreEntry = highScore[i];
+            nameStringBuilder.Append($"{scoreEntry.Name}:");
+            scoreStringBuilder.Append(scoreEntry.Score);
+            if (i <= highScore.Length - 1)
+            {
+                nameStringBuilder.Append("\n");
+                scoreStringBuilder.Append("\n");
+            }
+        }
+        _namesText.text = nameStringBuilder.ToString();
+        _scoresText.text = scoreStringBuilder.ToString();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Back();
+    }
+
+    private void Back()
+    {
+        Instantiate(_mainMenuPrefab, transform.parent);
+        Destroy(gameObject);
+    }
 }
--- Assets/Scripts/StateController.cs
+++ Assets/Scripts/StateController.cs
@@ -1,8 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection.Emit;
 
 public static class StateController
 {
     public static bool IsPaused = false;
     public static int CurrentVolume = 13;
+
+    public static ScoreEntry[] HighScore = 
+    {
+        new ScoreEntry("AAAA", 0),
+        new ScoreEntry("AAAA", 0),
+        new ScoreEntry("AAAA", 0),
+        new ScoreEntry("AAAA", 0),
+        new ScoreEntry("AAAA", 0),
+        new ScoreEntry("AAAA", 0),
+        new ScoreEntry("AAAA", 0)
+    };
+
+    public static void AddHighScore(string name, int score)
+    {
+        for (var i = 0; i < HighScore.Length; i++)
+        {
+            if (score > HighScore[i].Score)
+            {
+                for (var j = HighScore.Length - 1; j > i; j--)
+                    HighScore[j] = HighScore[j - 1];
+                HighScore[i] = new ScoreEntry(name.Substring(0, 4), score);
+                break;
+            }
+        }
+    }
 }
